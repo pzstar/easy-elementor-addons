86d69df734c165aadffdf1622e3890ea4c1511d6
Prabin Dangol <neprabin@gmail.com>
2024-12-10 17:21:56 +0545

    v 2.0.7

--- easy-elementor-addons.php
+++ easy-elementor-addons.php
@@ -4,7 +4,7 @@
  * Plugin Name: Easy Elementor Addons - Addons Pack for Elementor Page Builder Plugin
  * Plugin URI: https://demo.hashthemes.com/easy-elementor-addons/
  * Description: Elementor addons for WordPress Themes developed by HashThemes https://hashthemes.com
- * Version: 2.0.6
+ * Version: 2.0.7
  * Author: HashThemes
  * Author URI: https://hashthemes.com/
  * Text Domain: easy-elementor-addons
@@ -19,7 +19,7 @@ if (!defined('WPINC')) {
     die();
 }
 
-define('EEAD_VERSION', '2.0.6');
+define('EEAD_VERSION', '2.0.7');
 
 define('EEAD_FILE', __FILE__);
 define('EEAD_PLUGIN_BASENAME', plugin_basename(EEAD_FILE));
